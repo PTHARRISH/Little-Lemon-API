fe5b3bda253317b40ea4d83b61eaae27b6565ec1
PTHARRISH <harrishpt@gmail.com>
2025-01-06 23:25:02 +0530

    Pagination,Filter and ordering

--- LittleLemon/LittleLemonAPI/models.py
+++ LittleLemon/LittleLemonAPI/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
-    slug = models.SlugField(max_length=100, unique=True)
+    slug = models.SlugField()
     title = models.CharField(max_length=100)
 
     def __str__(self):
